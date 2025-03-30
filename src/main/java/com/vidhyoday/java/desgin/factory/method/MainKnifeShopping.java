adcf83e2991e5239f1e30cfabdd4923d98a80fab
Haresh Bhuriya <hareshybhuriya@gmail.com>
2025-03-30 22:36:23 +0530

    factory method uml diagram

--- src/main/java/com/vidhyoday/java/desgin/factory/method/MainKnifeShopping.java
+++ src/main/java/com/vidhyoday/java/desgin/factory/method/MainKnifeShopping.java
@@ -3,7 +3,7 @@ package com.vidhyoday.java.desgin.factory.method;
 public class MainKnifeShopping {
     public static void main(String[] args) {
         KnifeStore knifeStore = new BudgetKnifeStore();
-        knifeStore.createKnife("steak");
-        knifeStore.createKnife("chefs");
+        knifeStore.orderKnife("steak");
+        knifeStore.orderKnife("chefs");
     }
 }
